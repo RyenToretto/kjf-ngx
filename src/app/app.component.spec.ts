2b4c3b214ad5466bc0f859f75366941ddbef22b8
RyenToretto <18273727925@163.com>
2021-02-22 16:06:12 +0800

    build(): husky

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,11 +23,4 @@ describe('AppComponent', () => {
     it(`should have as title 'kjf-ngx'`, () => {
         const fixture = TestBed.createComponent(AppComponent);
     });
-
-    it('should render title', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.nativeElement;
-        expect(compiled.querySelector('.content span').textContent).toContain('kjf-ngx app is running!');
-    });
 });
